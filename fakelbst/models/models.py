b864d5d57c4c2af1f9885b8aadb70e34e20ddfc1
mao <myj226@gmail.com>
2013-02-09 19:02:16 +0800

    change to mongodb

--- fakelbst/models/models.py
+++ fakelbst/models/models.py
@@ -1,21 +1,44 @@
+#!/usr/bin/env python
+import os, json, sys, time
 from fakelbst import app
 from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, url_for, redirect, \
-     render_template, abort, g, flash, _app_ctx_stack
+     render_template, abort, g, flash, _app_ctx_stack, Response
+from fakelbst import db
 
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    top = _app_ctx_stack.top
-    if not hasattr(top, 'sqlite_db'):
-        top.sqlite_db = sqlite3.connect(app.config['DATABASE'])
-        top.sqlite_db.row_factory = sqlite3.Row
-    return top.sqlite_db
-
-def query_db(query, args=(), one=False):
-    """Queries the database and returns a list of dictionaries."""
-    cur = get_db().execute(query, args)
-    rv = cur.fetchall()
-    return (rv[0] if rv else None) if one else rv
-
+'''
+@app.route('/env')
+def env():
+    return os.getenv("VCAP_SERVICES", "{}")
+'''
+'''
+@app.route('/mongo')
+def mongotest():
+    from pymongo import Connection
+    uri = mongodb_uri()
+    conn = Connection(uri)
+    coll = conn.db['ts']
+    coll.insert(dict(now=int(time.time())))
+    last_few = [str(x['now']) for x in coll.find(sort=[("_id", -1)], limit=10)]
+    body = "\n".join(last_few)
+    return Response(body, content_type="text/plain;charset=UTF-8")
+'''
+'''
+def mongodb_uri():
+    local = os.environ.get("MONGODB", None)
+    if local:
+        return local
+    services = json.loads(os.getenv("VCAP_SERVICES", "{}"))
+    if services:
+        creds = services['mongodb-1.8'][0]['credentials']
+        uri = "mongodb://%s:%s@%s:%d/%s" % (
+            creds['username'],
+            creds['password'],
+            creds['hostname'],
+            creds['port'],
+            creds['db'])
+        print >> sys.stderr, uri
+        return uri
+    else:
+        raise Exception, "No services configured"
+'''
--- fakelbst/views/views.py
+++ fakelbst/views/views.py
@@ -1,9 +1,13 @@
+import urllib
+import os
 from fakelbst import app
-from flask import  render_template, Blueprint
+from flask import  render_template, Blueprint, redirect, url_for
 from fakelbst.models import models
+from fakelbst.models.mark import Mark
+from flask.views import MethodView
+from fakelbst.models.auth import *
 
 projects = Blueprint('fakelbst', __name__)
-app.config.from_pyfile('config.cfg', silent = True)
 
 @app.route('/')
 def welcome():
@@ -11,9 +15,8 @@ def welcome():
 
 @app.route('/mark')
 def markpage():
-    #if not g.user:
-        #return redirect(url_for('public_mark'))
-    return render_template('mark.html', weblink=models.query_db('''select * from mark '''))
+    marks = Mark.objects.all()
+    return render_template('mark.html', marks=marks)
 
 @app.route('/about')
 def aboutpage():
@@ -31,17 +34,36 @@ def login():
 def forcedirected():
     return render_template('/labs/ForceDirected.html')
 
+@app.route('/secret')
+@requires_auth
+def secret_page():
+    marks = Mark.objects.all()
+    return render_template('secret_page.html', marks=marks)
+
+@app.route('/add_mark', methods=['POST'])
+def add_mark():
+    import BeautifulSoup
+    url = request.form['weblink']
+    soup = BeautifulSoup.BeautifulSoup(urllib.urlopen(url))
+    title = soup.title.string
+    newMark = Mark(title=title, link=url, tag_id=1)
+    newMark.save()
+    return redirect(url_for("secret_page"))
+
+
+'''
 @app.route('/env')
 def env():
     return os.environ.get("VCAP_SERVICES", "{}")
-
+'''
+'''
 @app.teardown_appcontext
 def close_database(exception):
     """Closes the database again at the end of the request."""
     top = _app_ctx_stack.top
     if hasattr(top, 'sqlite_db'):
         top.sqlite_db.close()
-
+'''
 '''
 @app.before_request
 def before_request():
