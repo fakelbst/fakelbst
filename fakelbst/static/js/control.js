75603a56c6378dd053f3fbe4bb98490971f5e119
mao <myj226@gmail.com>
2013-02-11 13:40:48 +0800

    change the PaaS to openshift

--- fakelbst/static/js/control.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready(function(){
-    $('.nav li a').click(function(){
-        $(this).parent().parent().children().removeClass();
-        $(this).parent().addClass('active'); 
-    });
-});
